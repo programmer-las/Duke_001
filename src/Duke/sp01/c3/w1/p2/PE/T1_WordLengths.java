a55c952ab7b4b078be1d94d218f38a1efb22fa4b
nb4win10 <nb4win10@i.ua>
2017-02-15 22:03:52 +0200

    commit old files

---
+++ src/Duke/sp01/c3/w1/p2/PE/T1_WordLengths.java
@@ -0,0 +1,22 @@
+package Duke.sp01.c2.w1.p2.PE;
+
+import edu.duke.FileResource;
+
+/**
+ * Created by a on 1/27/17.
+ */
+
+public class T1_WordLengths {
+    public static void main(String[]args){
+        FileResource resource = new FileResource();
+        int counts[] = new int[10];
+
+        System.out.println(countWordLengths (resource, counts));
+    }
+
+    public static int[] countWordLengths (FileResource resource, int[] counts) {
+//        int[] counts;
+
+        return counts;
+    }
+}
---
+++ src/week04/duke1_c2_w4_v2_T0_babyNames01.java
@@ -0,0 +1,7 @@
+package week04;
+
+/**
+ * Created by a on 24.01.17.
+ */
+public class duke1_c2_w4_v2_T0_babyNames01 {
+}
